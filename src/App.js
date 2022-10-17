cf3bb5e0b8fa776abfdea78c05687fedb7abe6ab
noy2 <gameplay3542@icloud.com>
2022-10-17 23:14:11 +0900

    Modify: Added Prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import TodoList from './TodoList';
+import TodoList from "./TodoList";
 
 export default function App() {
-
   return (
     <div className="App">
       <p>TodoList1</p>
--- src/TodoListAddItem.js
+++ src/TodoListAddItem.js
@@ -1,15 +1,14 @@
-export default function TodoListAddItem({onCreate, text, handleChange}) {
+export default function TodoListAddItem({ onCreate, text, handleChange }) {
   return (
     <div>
-      <input 
-        className='addTodoInput' 
-        placeholder="Type here..." 
-        onChange={handleChange} 
-        value={text} />
-      <button 
-        className='addTodoButton' 
-        onClick={onCreate}>
-          Add to do
+      <input
+        className="addTodoInput"
+        placeholder="Type here..."
+        onChange={handleChange}
+        value={text}
+      />
+      <button className="addTodoButton" onClick={onCreate}>
+        Add to do
       </button>
     </div>
   );
