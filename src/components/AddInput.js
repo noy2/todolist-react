3108646377630b8e0d13b7f9e38c0fad5f9d12cb
noy2 <gameplay3542@icloud.com>
2022-10-20 14:13:06 +0900

    Add: Component Input for adding list

---
+++ src/components/AddInput.js
@@ -0,0 +1,24 @@
+import styled, { css } from "styled-components";
+
+const AddInput = styled.input`
+  font-size: 16px;
+  padding: 8px;
+  margin-right: 8px;
+  flex: none;
+  flex-grow: 1;
+  border: none;
+
+  &:active {
+    border: none;
+  }
+  &:focus {
+    border-radius: 4px;
+    outline: none;
+  }
+
+  &::placeholder {
+    color: #b4b4b4;
+  }
+`;
+
+export default AddInput;
